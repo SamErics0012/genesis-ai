0e9b9e46428fe11c1db038076b46f5ea25be0de7
Name <cosmiccreation699@gmail.com>
2025-11-28 15:22:09 +0000

    fox

--- types/spline-viewer.d.ts
+++ types/spline-viewer.d.ts
@@ -1,8 +1,14 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'spline-viewer': React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement> & { url?: string },
-      HTMLElement
-    >;
+import * as React from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'spline-viewer': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & { url?: string },
+        HTMLElement
+      >;
+    }
   }
 }
+
+export {};
