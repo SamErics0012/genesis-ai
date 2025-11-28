ddc4d51bdf73cca425d0fab8bbb3f6c12a95dd81
Name <cosmiccreation699@gmail.com>
2025-11-28 14:55:27 +0000

    Sam fix0001

--- app/image-generation/[model]/page.tsx
+++ app/image-generation/[model]/page.tsx
@@ -2,10 +2,12 @@
 
 import { ImageGenerator } from "@/components/image-generator";
 import Sidebar from "@/components/sidebar";
-import { useRouter } from "next/navigation";
+import { useRouter, useParams } from "next/navigation";
 
 export default function ImageGenerationPage() {
   const router = useRouter();
+  const params = useParams();
+  const modelSlug = params.model as string;
 
   const handleNavigate = (view: "home" | "video-gen" | "image-gen" | "account-settings" | "media-library" | "edit-image") => {
     if (view === "home") {
@@ -27,7 +29,7 @@ export default function ImageGenerationPage() {
     <div className="flex h-screen bg-background">
       <Sidebar onNavigate={handleNavigate} />
       <main className="flex-1 overflow-auto">
-        <ImageGenerator />
+        <ImageGenerator modelSlug={modelSlug} />
       </main>
     </div>
   );
